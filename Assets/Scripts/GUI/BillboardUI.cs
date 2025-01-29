58ec78b4ad4a153e14a2ebb9e793c969e50fe193
Anastasiia Mitinova <nastyawnastya@gmail.com>
2025-01-29 23:30:19 +0200

    Create BillboardUI, replace circle-progress-bar image

---
+++ Assets/Scripts/GUI/BillboardUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GUI
+{
+    public class BillboardUI : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+        }
+    }
+}

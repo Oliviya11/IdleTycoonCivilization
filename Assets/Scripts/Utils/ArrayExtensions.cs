33e8114af05aa2611c119f65d0ee27688af33b67
Anastasiia Mitinova <nastyawnastya@gmail.com>
2025-01-29 16:04:14 +0200

    Add array, list extensions

---
+++ Assets/Scripts/Utils/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assets.Scripts.Utils
+{
+    public static class ArrayExtensions
+    {
+        private static Random random = new Random();
+
+        public static void Shuffle(this Array array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                var value = array.GetValue(k);
+                array.SetValue(array.GetValue(n), k);
+                array.SetValue(value, n);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using SystemRandom = System.Random;
+
+namespace Assets.Scripts.Utils
+{
+    public static class ListExtensions
+    {
+        private static SystemRandom random = new SystemRandom();
+
+        public static T GetRandomElement<T>(this List<T> list)
+        {
+            int index = Random.Range(0, list.Count);
+            return list[index];
+        }
+
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+    }
+}
